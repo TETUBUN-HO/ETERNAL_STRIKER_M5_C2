ac9c7e0c06c2533e1bf252ca3d0bf7c35c8d632e
TETUBUN-HO <65408418+TETUBUN-HO@users.noreply.github.com>
2020-09-18 09:24:00 +0900

    Add files via upload

--- ETERNAL_STRIKER_M5_C2.ino
+++ ETERNAL_STRIKER_M5_C2.ino
@@ -1,6 +1,39 @@
 //
 //ETERNAL STRIKER Rr M5 CORE2
-//T.KATAKOTO TEAM REDHERRING/STUDIO Sequence 
+//T.KATAKOTO (TETUBUN-HO) TEAM Redherring(STUDIO Sequence) 
+//
+//ETERNAL_STRIKER_M5_C2 LICENSE: 
+//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvStartvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+//BSD 4-Clause License
+//
+//Copyright (c) 2020, TETUBUN-HO TEAM Redherring/STUDIO Sequence
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//1. Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//3. All advertising materials mentioning features or use of this software
+//   must display the following acknowledgement:
+//   This product includes software developed by the <organization>.
+//4. Neither the name of the <organization> nor the
+//   names of its contributors may be used to endorse or promote products
+//   derived from this software without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ''AS IS'' AND ANY
+//EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+//USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^End^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //
 #define C16(_rr,_gg,_bb) ((ushort)(((_rr & 0xF8) << 8) | ((_gg & 0xFC) << 3) | ((_bb & 0xF8) >> 3)))
 #define C8(_rr,_gg,_bb) ((ushort)(((_rr & 0xE0)) | ((_gg & 0xE0) >>3) | (_bb >> 6)))
@@ -10,7 +43,6 @@
 //#include "M5StackUpdater.h"
 #include "Sprite_t_k_cst.h"
 //
-#include <Wire.h>
 #include "SPIFFS.h"
 //
 #include "Resource.h"
